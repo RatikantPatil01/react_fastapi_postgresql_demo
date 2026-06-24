035b5a31409d2894a47b181892b67c1edce13978
Ratikant Patil <ratikantp71@gmail.com>
2026-06-24 21:03:42 +0530

    Successfully created hashed password storing and verifying code

--- backend/src/routers/user.py
+++ backend/src/routers/user.py
@@ -2,15 +2,18 @@ from fastapi import APIRouter, Depends, HTTPException
 from ..schemas.user import UserCreate
 from ..security.database import SessionLocal,get_db
 from ..models import User
+from ..security import auth
 
 
 router = APIRouter(prefix="/user",tags=['user'])
 
 @router.post("/login")
 def user_login(user:UserCreate,db= Depends(get_db)):
-    result = db.query(User).filter(User.username==user.username and User.password == user.password).first()
+    result = db.query(User).filter(User.username==user.username).first()
     if not result :
         raise HTTPException(status_code=404,detail="user not found.")
+    if not auth.verify_password(user.password,result.password):
+        raise HTTPException(status_code=404,detail="Invalid Credentials")
     if not result.is_active == True :
         raise HTTPException(status_code=404,detail="user is inactive")
     return {"Message":"Login Successfull","Data":result}
@@ -29,7 +32,8 @@ def get_user(id:int ,db = Depends(get_db)):
 
 @router.post("/add")
 def add_user(user:UserCreate,db = Depends(get_db)):
-    new_user = User(**user.model_dump())
+    # new_user = User(**user.model_dump())
+    new_user = User(username=user.username,password=auth.hash_password(user.password))
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
